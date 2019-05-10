78470b9d36a5e0e79a235f522b86d86a1d66cfdd
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-05-10 12:00:46 +0430

    Add project files.

---
+++ Art/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Art
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : BlurWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonConfig_Click(object sender, RoutedEventArgs e)
+        {
+            popupConfig.IsOpen = true;
+        }
+
+        private void CoverViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CoverViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
